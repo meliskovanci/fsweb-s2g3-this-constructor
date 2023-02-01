df0a283d8b32601afa1e9ca3eb894ca9eb12abc8
Melis Kovancı <meliskovanci@hotmail.com>
2023-02-01 20:33:25 +0300

    ödev tamamlandı

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 // Aşağıdaki görevleri tamamlayın
 
+const { transformFile } = require("@babel/core");
+
 /*
   GÖREV 1
     - Bir Kisi Constructor'ı yazın 'isim ve 'yas' argümanları alsın.
@@ -14,10 +16,30 @@
         + Bu `isim` ve `yas` i içeren bir string döndürmelidir Örnek: "Mary, 50"
 */
 
-function Kisi(/* kodlar buraya */) {
-  /* kodlar buraya */
-}
 
+function Kisi(isim,yas) {
+  this.isim = isim;
+  this.yas = yas;
+  this.mide = [];
+  this.ye = function(yemek){
+    
+    if (this.mide.length<11){
+      this.mide.push(yemek)
+    }
+  }
+  this.bosalt = function(){
+    this.mide =[];
+  }
+this.toString = function(){
+  return `${this.isim}, ${this.yas}`
+}
+}
+let name = new Kisi("İhsan", 40); 
+name.ye("patates")
+name.ye("hamburger")
+name.toString()
+
+   console.log(name.toString())
 
 /*
   GÖREV 2
@@ -35,18 +57,43 @@ function Kisi(/* kodlar buraya */) {
         +  "x milde benzinim bitti!" x değişkeni `odometer` daki sayı olmalıdır.
 */
 
-function Araba(/* kodlar buraya */) {
-  /* kodlar buraya */
+function Araba(model,tüketim) {
+  this.model = model;
+  this.milesPerGallon = tüketim;
+  this.tank = 0;
+  this.odometer = 0;
+  this.fill = function (galon){
+    this.tank+=galon;
+  }
+  this.drive = function (distance){
+    this.odometer += distance;
+    this.tank -= Math.round(distance/this.milesPerGallon);
+    if(this.tank <=0) {
+      const extraMile = this.tank * this.milesPerGallon;
+      this.odometer += Math.round(extraMile);
+      this.tank=0;
+      console.log( `${this.odometer} milde benzinim bitti!`)
+    }
+  }
+
 }
+ let VW = new Araba("passat",15);
+ VW.fill(65);
+ VW.drive(125);
+ VW.drive(950);
+
+ console.log(VW.tank);
+ console.log(VW.odometer);
+
 
 
 /* 
   GÖREV 3
   Kendi cümlelerinizle "this" kelimesinin 4 prensibini açıklayın:
-  1. 
-  2. 
-  3. 
-  4. 
+  1. Global Binding:Bir işlev bağımsız bir işlev olarak çağrıldığında bu tarayıcının penceresi ve küresel nesneyi ifade eder.
+  2. Implicit Binding: Bir işlev, yöntem olarak çağrıldığında bu yöntemin çağrıldığı nesneyi ifade eder.
+  3. Explicit Binding:Bunun değerini bir işlevin içinde call(), Apply() veya bind() yöntemlerini kullanarak açıkça ayarlayabiliriz.
+  4. New Binding: Bir fonksiyon new anahtar kelimesi ile çağrıldığında yeni bir nesne oluşturur ve bu yeni oluşturulan nesneyi ifade eder.
 */
 
 
